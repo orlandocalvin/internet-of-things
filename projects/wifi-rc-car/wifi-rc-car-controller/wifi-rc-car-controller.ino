6ad0113e778d2fff43763486c6c655e94e216f79
orlandocalvin <orlandocalvin46@gmail.com>
2025-02-10 02:40:53 +0700

    add wifi-rc-car-controller & modified wifi-rc-car-dev

---
+++ projects/wifi-rc-car/wifi-rc-car-controller/wifi-rc-car-controller.ino
@@ -0,0 +1,92 @@
+#include <Wire.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 32
+#define SAMPLE_COUNT 10
+#define RECEIVER_PORT 1234
+#define RECEIVER_IP "192.168.4.1"
+const char* SSID = "ESP8266 RC CAR";
+#define PASSWORD "zxcvbnmv"
+
+Adafruit_MPU6050 mpu;
+Adafruit_SSD1306 oled(OLED_WIDTH, OLED_HEIGHT, &Wire, -1);
+
+WiFiUDP udp;
+
+float accX_samples[SAMPLE_COUNT], accY_samples[SAMPLE_COUNT], accZ_samples[SAMPLE_COUNT]; // Buffer untuk filter moving average
+float accX_avg = 0, accY_avg = 0, accZ_avg = 0; // Variabel global untuk menyimpan rata-rata akselerasi
+int sampleIndex = 0;
+
+void getFilteredData() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  // Menyimpan data ke buffer moving average
+  accX_samples[sampleIndex] = a.acceleration.x;
+  accY_samples[sampleIndex] = a.acceleration.y;
+  accZ_samples[sampleIndex] = a.acceleration.z;
+  
+  sampleIndex = (sampleIndex + 1) % SAMPLE_COUNT;
+
+  // Menghitung rata-rata dari akselerasi
+  accX_avg = accY_avg = accZ_avg = 0;
+  for (int i = 0; i < SAMPLE_COUNT; i++) {
+    accX_avg += accX_samples[i];
+    accY_avg += accY_samples[i];
+    accZ_avg += accZ_samples[i];
+  }
+  accX_avg /= SAMPLE_COUNT;
+  accY_avg /= SAMPLE_COUNT;
+  accZ_avg /= SAMPLE_COUNT;
+}
+
+void setup() {
+  Serial.begin(115200);
+  mpu.begin();
+
+  WiFi.begin(SSID, PASSWORD);
+  delay(3000);
+
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  oled.setTextSize(1);
+  oled.setTextColor(WHITE);
+  oled.clearDisplay();
+  oled.setCursor(0,0);
+  oled.println("Can't Connect to \nReceiver");
+
+  if(WiFi.status() == WL_CONNECTED) {
+    oled.clearDisplay();
+    oled.setCursor(0,0);
+    oled.println("SSID: " + WiFi.SSID());
+    oled.println("\nIP: " + WiFi.localIP().toString());
+  }
+    oled.display();
+}
+
+void loop() {
+  StaticJsonDocument<256> data;
+  getFilteredData();
+
+  // Roll dan Pitch | satuan derajat | mau radian? hilangkan * 180 / PI
+  float roll = atan2(accY_avg, sqrt(accX_avg * accX_avg + accZ_avg * accZ_avg)) * 180 / PI;
+  float pitch = atan2(-accX_avg, accZ_avg) * 180 / PI;
+
+  // Menyusun JSON data yang akan dikirimkan
+  data["roll"] = roll;
+  data["pitch"] = pitch;
+
+  String jsonString;
+  serializeJson(data, jsonString);
+
+  udp.beginPacket(RECEIVER_IP, RECEIVER_PORT);
+  udp.print(jsonString);
+  udp.endPacket();
+
+  delay(15);
+}
--- projects/wifi-rc-car/wifi-rc-car-dev/wifi-rc-car-dev.ino
+++ projects/wifi-rc-car/wifi-rc-car-dev/wifi-rc-car-dev.ino
@@ -1,6 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
 
 // connections & values for drive Motors
 const int RIGHT_GAS = D1, LEFT_GAS = D2, RIGHT_DIR = D3, LEFT_DIR = D4;
@@ -17,6 +19,10 @@ const char* ssid = "ESP8266 RC CAR";
 const char* password = "zxcvbnmv";
 const int channel = 6, max_connections = 2;
 bool hidden = false;
+
+const int udpPort = 1234;
+WiFiUDP udp;
+char incomingPacket[256];
 
 void setup(){
   Serial.begin(74880);
@@ -43,6 +49,7 @@ void setup(){
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid, password, channel, hidden, max_connections);
   Serial.println("AP IP address: "+ WiFi.softAPIP().toString());
+  udp.begin(udpPort);
 
   // Mount LittleFS
   if (!LittleFS.begin()) {
@@ -76,7 +83,26 @@ void setup(){
 }
 
 void loop() {
-  // kosong?
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    int len = udp.read(incomingPacket, sizeof(incomingPacket) - 1);
+    if (len > 0) {
+      incomingPacket[len] = '\0'; // Null-Terminator
+    }
+
+    // Parsing JSON
+    StaticJsonDocument<256> data;
+    deserializeJson(data, incomingPacket);
+
+    float roll = data["roll"];
+    float pitch = data["pitch"];
+
+    if (pitch > 40 && pitch < 100) Forward();
+    else if (pitch < -40 && pitch > -100) Backward();
+    else if (roll > 40 && roll < 100) TurnRight();
+    else if (roll < -40 && roll > -100) TurnLeft();
+    else Stop();
+  }
 }
 
 void Throttle(int gas) {
