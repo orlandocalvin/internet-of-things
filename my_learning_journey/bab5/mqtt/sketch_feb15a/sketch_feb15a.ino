cfa60503ca8cefcc8ee143234b8f5357c011cc6d
orlandocalvin <orlandocalvin46@gmail.com>
2025-02-16 01:11:23 +0700

    prepare mqtt

--- my_learning_journey/bab5/mqtt/sketch_feb15a/sketch_feb15a.ino
+++ my_learning_journey/bab5/mqtt/sketch_feb15a/sketch_feb15a.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LED_PIN 2
+#define BUTTON_PIN 15
 
+bool ledState = LOW, lastButtonState = HIGH;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool buttonState = digitalRead(BUTTON_PIN);
 
+  if (buttonState == LOW && lastButtonState == HIGH) {
+    digitalWrite(LED_PIN, ledState = !ledState);
+    Serial.println(ledState ? "LED ON" : "LED OFF");
+    delay(300);
+  }
+
+  lastButtonState = buttonState;
 }
